50a54fdfef1a369f2f71136c075269158c35dde3
AliakseiSafonau <pharmaceftssma@gmail.com>
2022-06-24 22:15:11 +0300

    feat: add problem solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+  if (n < 0) {
+    n = n * -1;
+  }
+
+  let x = '';
+  for ( let i = 0; i < String(n).length; i++) {
+    x = x + String(n)[String(n).length - 1 - i];
+  }
+
+  return Number(x);
+};
